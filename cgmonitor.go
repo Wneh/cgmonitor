2fbd2f92e244e20699b0b65ce003fcd7ff9095df
Carl <carle293@hotmail.com>
2013-06-09 15:44:33 +0200

    Added config file

--- cgmonitor.go
+++ cgmonitor.go
@@ -3,12 +3,37 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/BurntSushi/toml"
 	"net"
 )
 
+type tomlConfig struct {
+	Miners map[string]miner
+}
+
+type miner struct {
+	IP string
+}
+
 func main() {
 
+	//Start by reading the config file
+	var config tomlConfig
+	_, err := toml.DecodeFile("config.toml", &config)
+	//Check for errors
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for minerName, miner := range config.Miners {
+		fmt.Printf("Server: %s(%s)\n", minerName, miner.ip)
+	}
+
 	conn, err := net.Dial("tcp", "192.168.1.102:4028")
+
+	c := Client{"Foo", "127.0.0.1", conn, 10}
+	fmt.Println("Client: ", c)
 
 	if err != nil {
 		panic(err)
